9b198998904eb76df1d022fb1c0b5edbb5398a0a
yuya.wakayama <uya.no18@gamil.com>
2018-06-26 06:42:29 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
-    resirect_to tasks_url
+    redirect_to tasks_url
   end
   
   private
